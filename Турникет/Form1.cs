4f8b99f74759cf8fd1eb3e5b548c159fe482065d
Robert Bauer <robert.bauer202020@gmail.com>
2023-09-20 10:54:04 +0500

    20.09.2023

---
+++ Турникет/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Турникет
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sheme_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Photo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
